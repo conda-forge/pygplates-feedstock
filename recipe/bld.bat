34433e7e6ed223132475a569578fdf1e974d8b0c
John Cannon <john.cannon@sydney.edu.au>
2026-05-04 23:00:22 +1000

    Attempt to fix heap space error by disabling parallel build on Windows.
    This is similar to f21aab7, but we don't need to remove the /MP switch
    in the source code (because setting GPLATES_MSVC_PARALLEL_BUILD to FALSE
    now does this for us in pygplates 1.0 source code).

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,7 @@
 :: Note that Boost_ROOT helps avoid finding the Boost library using inherited env var PATH
 ::      (which can reference a Boost outside of conda). Also, CGAL looks for Boost too.
 %PYTHON% -m pip install -vv ^
-      -C cmake.define.GPLATES_MSVC_PARALLEL_BUILD=TRUE ^
-      -C cmake.define.GPLATES_MSVC_PARALLEL_BUILD_PROCESSES=%CPU_COUNT% ^
+      -C cmake.define.GPLATES_MSVC_PARALLEL_BUILD=FALSE ^
       -C "cmake.define.CMAKE_PREFIX_PATH=%PREFIX%;%LIBRARY_PREFIX%" ^
       -C "cmake.define.Boost_ROOT=%LIBRARY_PREFIX%" ^
       "%SRC_DIR%"
